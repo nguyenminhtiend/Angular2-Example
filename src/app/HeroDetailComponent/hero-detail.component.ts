463659068afd275989db3ea2eb9aa0f2b7e1603b
Tien Nguyen <nguyenminhtiend@gmail.com>
2017-06-21 01:22:59 +0700

    #

--- src/app/HeroDetailComponent/hero-detail.component.ts
+++ src/app/HeroDetailComponent/hero-detail.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 
-import {Hero} from '../hero'
+import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
